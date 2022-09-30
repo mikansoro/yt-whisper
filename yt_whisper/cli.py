be2928e4aca1552538856bd1b5b74268bf1e06b4
Jing Hua <tohjinghua123@gmail.com>
2022-09-30 12:22:02 +0800

    feat: manual language selection

--- yt_whisper/cli.py
+++ yt_whisper/cli.py
@@ -32,6 +32,9 @@ def main():
 
     parser.add_argument("--break-lines", type=int, default=0, 
                         help="Whether to break lines into a bottom-heavy pyramid shape if line length exceeds N characters. 0 disables line breaking.")
+    
+    parser.add_argument("--language", type=str, default=None, choices=sorted(LANGUAGES.keys()) + sorted([k.title() for k in TO_LANGUAGE_CODE.keys()]), 
+                        help="language spoken in the audio, specify None to perform language detection")
 
     args = parser.parse_args().__dict__
     model_name: str = args.pop("model")
